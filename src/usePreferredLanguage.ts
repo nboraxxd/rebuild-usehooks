14b067c0e9f8e1d032bff9825cbb35f258e50ad2
bora <nhatbaoxxd@gmail.com>
2023-12-31 17:30:56 +0700

    Solution

--- src/usePreferredLanguage.ts
+++ src/usePreferredLanguage.ts
@@ -1,3 +1,18 @@
+import { useSyncExternalStore } from 'react'
+
+const subscribe = (cb: () => void) => {
+  window.addEventListener('languagechange', cb)
+  return () => window.removeEventListener('languagechange', cb)
+}
+
+const getSnapshot = () => {
+  return window.navigator.language
+}
+
+const getPreferredLanguageServerSnapshot = () => {
+  throw Error('usePreferredLanguage is a client-only hook')
+}
+
 export default function usePreferredLanguage() {
-  return 'zh-HK'
+  return useSyncExternalStore(subscribe, getSnapshot, getPreferredLanguageServerSnapshot)
 }
