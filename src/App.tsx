87c1c1457b9686c02d659021760aa60895c5dac8
bora <nhatbaoxxd@gmail.com>
2023-12-31 11:45:53 +0700

    Problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
-function App() {
-  return <h1>Hello there!</h1>
-}
+import * as React from 'react'
+import useDocumentTitle from '@/useDocumentTitle'
 
-export default App
+export default function App() {
+  const [count, setCount] = React.useState(0)
+
+  const handleClick = () => setCount(count + 1)
+
+  useDocumentTitle(`Clicked ${count} times.`)
+
+  return (
+    <section>
+      <h1>useDocumentTitle</h1>
+      <button className="primary" onClick={handleClick}>
+        Increment Count: {count}
+      </button>
+
+      <p>
+        You won't be able to see the changes if you're in a sandbox environment. Instead, you'll want to open up the app{' '}
+        <a className="link" href="https://3gyk2d.csb.app/" target="_blank" rel="noreferrer">
+          in a new tab and paste your code into it
+        </a>
+        .
+      </p>
+    </section>
+  )
+}
